97619b84fb40486b88ad9fdbf4feca0ab89a0e36
Drako-vic <155984085+Drako-vic@users.noreply.github.com>
2024-01-08 15:03:36 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,2 @@
+music.play(music.string_playable("A E G B C5 F D C ", 121),
+    music.PlaybackMode.UNTIL_DONE)
---
+++ main.ts
@@ -0,0 +1,1 @@
+music.play(music.stringPlayable("A E G B C5 F D C ", 121), music.PlaybackMode.UntilDone)
